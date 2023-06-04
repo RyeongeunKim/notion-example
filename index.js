850fc2a1788ea1a11a83720c102be4c316ea9497
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-06-04 10:27:11 +0900

    노션 db 조회

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import { Client } from '@notionhq/client';
+import { Client } from "@notionhq/client";
 
-const notion = new Client({ auth: process.env.NOTION_TOKEN });
-// const notion = new Client({ auth: process.env.NOTION_KEY });
+const notion = new Client({ auth: process.env.NOTION_KEY });
 const databaseId = process.env.NOTION_DATABASE_ID;
 
 /** 삽입 테스트 */
@@ -22,21 +21,31 @@ async function addItem(text) {
       },
     });
     console.log(response);
-    console.log('Success! Entry added.');
+    console.log("Success! Entry added.");
   } catch (error) {
     console.error(error.body);
   }
 }
 
-addItem('삽입 테스트');
-addItem('INSERT');
+// addItem('20230604');
+// addItem('삽입테스트');
 
-/** API 요청하기 */
-// const getUsers = async () => {
-//   const listUsersResponse = await notion.users.list({});
-//   console.log('listUsersResponse = ', listUsersResponse);
-// };
+// 노션 DB 조회하기
+let url = "https://api.notion.com/v1/pages/1523068ab59042578f40bc1d1e960468";
+let method = 'GET'
 
-// getUsers();
+const response = await fetch(url, {
+  method: method,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.NOTION_KEY}`,
+    "Notion-Version": "2022-06-28",
+  },
+  // body: JSON.stringify(data),
+});
+
+const resData = await response.json();
+
+console.log(resData);
 
 
